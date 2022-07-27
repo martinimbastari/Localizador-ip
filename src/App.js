fa5f0b62332775b52dc25e2685ac20f45036b976
martin <martin.imbastari98@gmail.com>
2022-07-27 17:12:12 -0300

    agregue el mapa

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
 import React, {useState, useEffect} from 'react';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import style from  './App.module.css';
+import 'leaflet/dist/leaflet.css'
 import arrow from './assets/icon-arrow.svg'
+import icon from './assets/icon-location.svg'
+
 
 function App() {
 const [ip, setIp] = useState('');
 const [location, setLocation] = useState('');
 const [timezone, setTimezone] = useState('');
 const [isp, setIsp] = useState('');
-console.log(ip)
-console.log(location)
+const [lat, setLat] = useState('');
+const [lng, setLng] = useState('');
+let valorIngresado = '';
 
-const url = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_DCOFJA1D6QwoGZyzuW6Z4oOUCio9h'
+
+
+const url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_IbVBG1ro8zey9jOQv6IzKP1MXOpYb&ipAddress=${ip}`
 let ubicacion = '';
 
-useEffect(() => {
-  (async function () {
-    let data = await fetch(url).then((res) => res.json());
-    setIp(data.ip);
-    ubicacion = data.location.city + ', ' + data.location.region;
-    setLocation(ubicacion)
-    setTimezone(data.location.timezone)
-    setIsp(data.isp)
-  })();
-}, [url]);
+// useEffect(() => {
+//   (async function () {
+//     let data = await fetch(url).then((res) => res.json());
+//     setIp(data.ip);
+//     ubicacion = data.location.city + ', ' + data.location.region;
+//     setLocation(ubicacion)
+//     console.log(ubicacion)
+//     setTimezone(data.location.timezone)
+//     setIsp(data.isp)
+//     setLat(data.location.lat)
+//     setLng(data.location.lng)
+//   })();
+// }, [url]);
+
+
+const handleChange = e => {
+  e.preventDefault();
+   return valorIngresado = e.target.value;
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setIp(valorIngresado)
+}
 
   return (
+    <>
     <div className={style.container}>
       <div className={style.contenedorInput}>
         <h1>Localizador IP</h1>
-        <input/>
-        <button> <img src={arrow}/> </button>
+        <form onSubmit={handleSubmit}>
+          <input type='text'  onChange={handleChange} />
+          <button type='submit' ><img src={arrow}/></button>
+        </form>
       </div>
       <div className={style.contenedorInfo}>
         <div>
-          <p>direccion ip</p>
+          <p>Direccion ip</p>
           <h2>{ip}</h2>
         </div>
         <div>
@@ -49,8 +73,21 @@ useEffect(() => {
           <h2>{isp}</h2>
         </div>
       </div>
-    {/* <Mapview/> */}
-    </div>
+      </div>
+      
+      <MapContainer center={{lat,lng}} style={{width:'100vw', height: '80vh'}} zoom={5} scrollWheelZoom={true}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={{lat, lng}} >
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+    </>
+    
   );
 }
 
